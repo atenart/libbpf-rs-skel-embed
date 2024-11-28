4d1aa57e904699a593b0445ef452c4a3aa298331
Antoine Tenart <atenart@redhat.com>
2024-11-28 11:35:42 +0100

    Update libbpf-rs to 0.24
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- src/manager.rs
+++ src/manager.rs
@@ -1,32 +1,40 @@
+use std::mem::MaybeUninit;
+
 use anyhow::{anyhow, Result};
-use libbpf_rs::skel::SkelBuilder;
+use libbpf_rs::skel::{OpenSkel, Skel, SkelBuilder};
 
 mod bpf {
     include!("bpf/kprobe.skel.rs");
 }
 use bpf::*;
 
-pub(crate) struct KprobeManager {
-    obj: libbpf_rs::Object,
+pub(crate) struct KprobeManager<'a> {
+    storage: Box<MaybeUninit<libbpf_rs::OpenObject>>,
+    skel: KprobeSkel<'a>,
     links: Vec<libbpf_rs::Link>,
 }
 
-impl KprobeManager {
+impl KprobeManager<'_> {
     pub(crate) fn new() -> Result<Self> {
-        let mut skel = KprobeSkelBuilder::default().open()?;
+        let mut storage = Box::new(MaybeUninit::uninit());
+        let skel = KprobeSkelBuilder::default().open(&mut storage)?;
 
         // Set rodata.
-        skel.rodata_mut().log_level = 0;
+        skel.maps.rodata_data.log_level = 0;
 
         Ok(Self {
-            obj: skel.obj.load()?,
+            storage,
+            skel: skel.load()?,
             links: Vec::new(),
         })
     }
 
     pub(crate) fn attach(&mut self, probe: &str) -> Result<()> {
         self.links.push(
-            self.obj.prog_mut("kprobe_placeholder")
+            self.skel
+                .object()
+                .progs_mut()
+                .find(|p| p.name() == "kprobe_placeholder")
                 .ok_or_else(|| anyhow!("Couldn't get program"))?
                 .attach_kprobe(false, probe)?,
         );
